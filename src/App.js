1b6bfca0d2afcf1ee98ba9ffa85ce8c49b817554
adela05 <aesparza051@gmail.com>
2018-11-14 20:13:28 -0600

    my initial commit for todolist

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import NewTodoForm from './NewTodoForm';
+import TodoList from './TodoList';
+import './App.css';
+
+class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      message: 'Hello Amigos!',
+      newTodo: ' ',
+      todos: [{
+        title: 'hi',
+        done: false
+      },{
+        title: 'bye',
+        done: false
+      } ]
+    };
+  }
+  newTodoChanged(event) {
+      this.setState({
+      newTodo: event.target.value
+    });
+  }
+  formSubmitted(event) {
+    event.preventDefault();
+    this.setState({
+      newTodo: ' ',
+    todos: [...this.state.todos, {
+      title: this.state.newTodo,
+      done: false
+    }]
+  });
+  }
+
+  toggleTodoDone(event, index) {
+    const todos = [...this.state.todos]; // copy the array
+    todos [index] = {
+      ...todos [index],
+      done: event.target.checked  // update done property on copied
+    }; // copy the todo can also use Object.assign
+
+      this.setState({
+        todos
+    });
+  }
+
+  removeTodo(index) {
+    const todos = [...this.state.todos];
+    todos.splice(index, 1);
+    this.setState({
+      todos
+    });
+  }
+
+allDone() {
+  const todos= this.state.todos.map(todo => {
+    return {
+      title: todo.title,  //can also do ...todo
+      done: true
+    };
+  });
+  this.setState({
+    todos
+  });
+}
+
+  render() {
+    return (
+      <div className="App">
+        <h3>{this.state.message}</h3>
+        <NewTodoForm
+        newTodo={this.state.newTodo}
+        formSubmitted={this.formSubmitted.bind(this)}
+        newTodoChanged={this.newTodoChanged.bind(this)} />
+        <button onClick={() => this.allDone()}>All Done</button>
+        <TodoList
+        todos={this.state.todos}
+        toggleTodoDone={this.toggleTodoDone.bind(this)}
+        removeTodo={this.removeTodo.bind(this)} />
+
+      </div>
+    );
+  }
+}
+
+export default App;
